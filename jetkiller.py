68937e7e23c6ee2355ad75115ee50a704412474e
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2018-09-12 22:38:16 +0200

    Light refactoring and documentation.

--- jetkiller.py
+++ jetkiller.py
@@ -10,36 +10,20 @@ internal_mode = "RGB"
 internal_type = float
 
 
+def jetkiller(input_filename, output_filename="output.png"):
+    """Convert an image file from the "jet" to the "viridis" colormap."""
+    input_image_data = read_image(input_filename)
+    input_cmap = jet_map()
+    output_cmap = viridis_map()
+    output_image_data = convert_image(input_image_data, input_cmap, output_cmap)
+    write_image(output_filename, output_image_data)
+
+
 def read_image(input_filename):
     """Read an image from a file."""
     im = Image.open(input_filename)
     im_converted = im.convert(mode=internal_mode)
     return im_converted
-
-
-def write_image(output_filename, im):
-    """Save an image to a file."""
-    im.save(output_filename)
-
-
-def viridis_map():
-    """Return the "virdis" colormap."""
-    cmap = np.round(np.array(cm.get_cmap("viridis").colors) * 256)
-    return np.array(cmap, dtype=internal_type)
-
-
-def jet_map():
-    """Return the "jet" colormap."""
-    n = 256
-    r = mpl.colors.makeMappingArray(256, cm.jet._segmentdata['red']) * n
-    g = mpl.colors.makeMappingArray(256, cm.jet._segmentdata['green']) * n
-    b = mpl.colors.makeMappingArray(256, cm.jet._segmentdata['blue']) * n
-    data = np.zeros_like(viridis_map())
-    for i in range(len(r)):
-        data[i, 0] = r[i]
-        data[i, 1] = g[i]
-        data[i, 2] = b[i]
-    return np.array(np.around(data), dtype=internal_type)
 
 
 def convert_image(im, input_cmap, output_cmap):
@@ -68,10 +52,26 @@ def convert_image(im, input_cmap, output_cmap):
     return Image.fromarray(data, mode=internal_mode)
 
 
-def jetkiller(input_filename, output_filename="output.png"):
-    """Convert an image file's colormap from "jet" to "viridis"."""
-    input_image_data = read_image(input_filename)
-    input_cmap = jet_map()
-    output_cmap = viridis_map()
-    output_image_data = convert_image(input_image_data, input_cmap, output_cmap)
-    write_image(output_filename, output_image_data)
+def write_image(output_filename, im):
+    """Save an image to a file."""
+    im.save(output_filename)
+
+
+def viridis_map():
+    """Return the "virdis" colormap as an array."""
+    cmap = np.round(np.array(cm.get_cmap("viridis").colors) * 256)
+    return np.array(cmap, dtype=internal_type)
+
+
+def jet_map():
+    """Return the "jet" colormap as an array."""
+    n = 256
+    r = mpl.colors.makeMappingArray(256, cm.jet._segmentdata['red']) * n
+    g = mpl.colors.makeMappingArray(256, cm.jet._segmentdata['green']) * n
+    b = mpl.colors.makeMappingArray(256, cm.jet._segmentdata['blue']) * n
+    data = np.zeros_like(viridis_map())
+    for i in range(len(r)):
+        data[i, 0] = r[i]
+        data[i, 1] = g[i]
+        data[i, 2] = b[i]
+    return np.array(np.around(data), dtype=internal_type)
