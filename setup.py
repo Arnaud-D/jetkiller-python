7564c9a0c17cf8876d6f0411068d80179b289ff0
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2018-09-15 10:14:38 +0200

    First version of the install script.

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jetkiller",
+    version="0.6.3",
+    author="Arnaud-D",
+    description="Enhance data visualisations using the \"jet\" colormap.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Arnaud-D/jetkiller",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=["pillow", "numpy", "matplotlib"],
+)
