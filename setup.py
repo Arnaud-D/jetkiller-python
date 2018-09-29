e59566ab5946bb517c120e4adec0b99e70086853
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2018-09-29 22:43:45 +0200

    Update setup.py for v0.9.1 (package fix).

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jetkiller",
-    version="0.9.0",
+    version="0.9.1",
     author="Arnaud-D",
     description="Enhance data visualizations using the \"jet\" colormap.",
     long_description=long_description,
