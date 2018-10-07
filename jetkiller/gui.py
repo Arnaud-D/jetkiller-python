e92dff0e7f399ea8ba172154e5f5f44ab2684d91
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2018-10-07 12:16:38 +0200

    Refactor.

--- jetkiller/gui.py
+++ jetkiller/gui.py
@@ -7,6 +7,7 @@ import sys
 
 
 def scaling_transform(w1, h1, w2, h2):
+    """Rescale rectangle (w1, h1) to fit in rectangle (w2, h2) without changing the ratio."""
     r1 = w1 / h1
     r2 = w2 / h2
     if r1 <= r2:
@@ -19,6 +20,7 @@ def scaling_transform(w1, h1, w2, h2):
 
 
 def update_preview(image, preview_canvas):
+    """Use an image to update a preview canvas."""
     frame_width, frame_height = int(preview_canvas["width"]), int(preview_canvas["height"])
     w1, h1 = image.width, image.height
     w, h = scaling_transform(w1, h1, frame_width, frame_height)
@@ -30,6 +32,7 @@ def update_preview(image, preview_canvas):
 
 
 def update_previews(input_1, menu_var, input_preview_canvas, output_preview):
+    """Update the input and output preview canvases from interface data."""
     # TODO: investigate problem with alpha channel
 
     try:
@@ -47,6 +50,7 @@ def update_previews(input_1, menu_var, input_preview_canvas, output_preview):
 
 
 def browse_click(input_1, cm_menu, input_preview, output_preview):
+    """Action when clicking on the browse button."""
     file = fd.askopenfilename()
     if file != ():
         input_1.delete(0, last=tk.END)
@@ -55,6 +59,7 @@ def browse_click(input_1, cm_menu, input_preview, output_preview):
 
 
 def save_click(input_1, v):
+    """Action when clicking on the save button."""
     filename = fd.asksaveasfilename()
     try:
         jk.convert_file(input_1.get(), filename, colormap=v.get())
@@ -69,39 +74,47 @@ def main():
     root.title("Jet Killer")
 
     # Preview of input file
-    input_preview = tk.Canvas(root, width=300, height=300)
-    input_preview.grid(column=0, row=2, columnspan=2)
+    input_preview_canvas = tk.Canvas(root, width=300, height=300)
+    input_preview_canvas.grid(column=0, row=2, columnspan=2)
 
     # Preview of output file
-    output_preview = tk.Canvas(root, width=300, height=300)
-    output_preview.grid(column=2, row=2, columnspan=2)
+    output_preview_canvas = tk.Canvas(root, width=300, height=300)
+    output_preview_canvas.grid(column=2, row=2, columnspan=2)
 
-    # File choice
-    label_1 = tk.Label(root, text="Input file:")
-    label_1.grid(column=0, row=0)
-    input_1 = tk.Entry(root)
-    input_1.grid(column=1, row=0)
-    button_1 = tk.Button(root, text="...")
-    button_1.grid(column=2, row=0)
+    # Input file entry
+    input_file_prompt = tk.Label(root, text="Input file:")
+    input_file_prompt.grid(column=0, row=0)
+    input_file_entry = tk.Entry(root)
+    input_file_entry.grid(column=1, row=0)
+    input_file_browse_button = tk.Button(root, text="...")
+    input_file_browse_button.grid(column=2, row=0)
 
-    # Colormap choice
-    label_2 = tk.Label(root, text="Colormap:")
-    label_2.grid(column=0, row=1)
-    menu_var = tk.StringVar()
-    options = ("viridis", "plasma", "inferno", "magma")
-    menu_var.set("viridis")
+    # Colormap menu
+    colormap_prompt = tk.Label(root, text="Colormap:")
+    colormap_prompt.grid(column=0, row=1)
+    colormap_var = tk.StringVar()
+    colormap_options = ("viridis", "plasma", "inferno", "magma")  # TODO: add other  colormap choices for matplotlib
+    colormap_var.set("viridis")
+    colormap_menu = tk.OptionMenu(root, colormap_var, *colormap_options)
+    colormap_menu.grid(column=1, row=1, columnspan=2)
 
-    cm_menu = tk.OptionMenu(root, menu_var, *options)
-    cm_menu.grid(column=1, row=1, columnspan=2)
-    cm_menu.bind('<<OptionMenuSelect>>', update_previews)
+    # Save button
+    save_button = tk.Button(root, text="Convert and save")
+    save_button.grid(column=3, row=0, rowspan=2, sticky=tk.N+tk.S)
 
-    # Convert and save button
-    button_2 = tk.Button(root, text="Convert and save")
-    button_2.grid(column=3, row=0, rowspan=2, sticky=tk.N+tk.S)
+    # Actions
+    def input_file_browse_button_action():
+        return browse_click(input_file_entry, colormap_var, input_preview_canvas, output_preview_canvas)
 
-    button_1["command"] = lambda: browse_click(input_1, menu_var, input_preview, output_preview)
-    button_2["command"] = lambda: save_click(input_1, menu_var)
-    menu_var.trace("w", lambda *args: update_previews(input_1, menu_var, input_preview, output_preview))
+    def save_button_action():
+        return save_click(input_file_entry, colormap_var)
+
+    def colormap_action(*args):
+        return update_previews(input_file_entry, colormap_var, input_preview_canvas, output_preview_canvas)
+
+    input_file_browse_button["command"] = input_file_browse_button_action
+    save_button["command"] = save_button_action
+    colormap_var.trace("w", colormap_action)
 
     # Main loop
     root.mainloop()
