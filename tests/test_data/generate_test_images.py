2630ccf6833d4dc2250f511e19b868b6dfbcb2ed
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2018-10-28 08:19:46 +0100

    Increase detail level in test images.

--- tests/test_data/generate_test_images.py
+++ tests/test_data/generate_test_images.py
@@ -1,18 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def export_figure(colormap):
     x = np.linspace(-np.pi, np.pi, 100)
     y = np.linspace(-np.pi, np.pi, 100)
     xv, yv = np.meshgrid(x, y)
     z = np.sin(xv) * np.sin(yv)
-    plt.contourf(x, y, z, 30)
+    plt.contourf(x, y, z, 200)
     plt.set_cmap(colormap)
     plt.savefig('test_image_' + colormap + '.png')
+
 
 def main():
     export_figure("jet")
     export_figure("viridis")
+
 
 if __name__ == "__main__":
     main()
